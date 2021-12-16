40c5d55fbf979facdf7d26223edab9e9a9b13cf6
Jayant Dhawan <jayantdhawan95@gmail.com>
2021-12-17 00:49:20 +0530

    Add tests

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,10 +1,25 @@
 from fastapi.testclient import TestClient
+from pathlib import Path
 
 from app.main import app
 
 client = TestClient(app)
 
 
-def test_home_page():
+def test_home_page_route_exist():
     response = client.get("/")
     assert response.status_code == 200
+
+def test_number_plate_route_exist():
+    response = client.post(
+        '/read-plate',
+        files={"file": ("car.jpeg", open(str(Path().absolute()) + '/tests/test_images/car.jpeg', "rb"), "image/jpeg")}
+    )
+    assert response.status_code == 200
+
+def test_read_number_plate_from_image():
+    response = client.post(
+        '/read-plate',
+        files={"file": ("car.jpeg", open(str(Path().absolute()) + '/tests/test_images/car.jpeg', "rb"), "image/jpeg")}
+    )
+    assert response.json() == 'DL8CAF5030'
