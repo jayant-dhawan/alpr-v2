dcd08e936fd2fd79240041a8a9849f961fa46e49
Jayant Dhawan <jayantdhawan95@gmail.com>
2021-12-17 00:08:51 +0530

    Add homepage

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ from pydantic import AnyHttpUrl, BaseSettings, validator
 class Settings(BaseSettings):
     PROJECT_NAME: str
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    TESSERACT_PATH: str
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
